b50108cdce7b9e293a5f7a9433e87c363bb18468
Mujtaba12-al <mujtaba@mativeinc.com>
2025-04-04 22:41:40 +0500

    script settings1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import User from "../models/userSchema.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 
 export const registerUser = async (req, res) => {
